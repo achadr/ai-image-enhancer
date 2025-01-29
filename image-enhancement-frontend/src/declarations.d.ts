494ed78cd36bb385ba097fddb11b1a17d4ad03ff
Achraf <achraf.bougattaya@gmail.com>
2025-01-29 16:13:47 +0100

    Initial Project setup

---
+++ image-enhancement-frontend/src/declarations.d.ts
@@ -0,0 +1,15 @@
+// Declare modules for non-TypeScript files
+declare module '*.svg' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.png' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.jpg' {
+    const content: string;
+    export default content;
+  }
